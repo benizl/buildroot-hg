673ca6728ba4690c8ac35729420fe8f8013ea785
Ben Nizette <benjamin.nizette@anu.edu.au>
2016-07-04 12:41:53 +1000

    Move to newer CherryPy version

--- package/python-cherrypy/python-cherrypy.mk
+++ package/python-cherrypy/python-cherrypy.mk
@@ -4,11 +4,17 @@
 #
 ################################################################################
 
-PYTHON_CHERRYPY_VERSION = 4.0.0
+PYTHON_CHERRYPY_VERSION = 5.1.0
 PYTHON_CHERRYPY_SOURCE = CherryPy-$(PYTHON_CHERRYPY_VERSION).tar.gz
-PYTHON_CHERRYPY_SITE = http://pypi.python.org/packages/source/C/CherryPy
-PYTHON_CHERRYPY_LICENSE = BSD-3c
-PYTHON_CHERRYPY_LICENSE_FILES = cherrypy/LICENSE.txt
-PYTHON_CHERRYPY_SETUP_TYPE = distutils
+PYTHON_CHERRYPY_SITE = https://pypi.python.org/packages/source/C/CherryPy
+PYTHON_CHERRYPY_LICENSE = BSD
+PYTHON_CHERRYPY_LICENSE_FILES = LICENSE
+PYTHON_CHERRYPY_SETUP_TYPE = setuptools
+
+define PYTHON_CHERRYPY_REMOVE_TESTS
+	rm -r $(TARGET_DIR)/usr/lib/python2.7/site-packages/cherrypy/test
+endef
+
+PYTHON_CHERRYPY_POST_INSTALL_TARGET_HOOKS += PYTHON_CHERRYPY_REMOVE_TESTS
 
 $(eval $(python-package))
